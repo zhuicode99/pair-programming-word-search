a63e7725b542a9bb85369b615e9e831507939e52
zhuicode99 <enlin291814833@hotmail.com>
2022-08-31 20:34:26 +0000

    w2d3 pair

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,35 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARRY')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'NOTEXIST')
+
+    assert.isFalse(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
+const transpose = function(matrix) {
+    let result = [];
+    for (let row = 0; row < matrix[0].length; row ++) {
+      let tmpList = [];
+      for (let col = 0; col < matrix.length; col ++) {
+        tmpList.push(matrix[col][row]);
+      }
+      result.push(tmpList);
+    }
+    return result;
+  };
+
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+    const verticalJoin = transpose(letters).map(item => item.join(''))
+
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    for (item of verticalJoin) {
+        if (item.includes(word)) return true
+    }
+    return false;
 }
 
 module.exports = wordSearch
